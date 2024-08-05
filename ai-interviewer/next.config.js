3c43abd20a6c95d57408660304ffeca017124719
Yashism <yashvwaghmare2002@gmail.com>
2024-08-04 23:52:28 -0500

    vercel.json implementation

--- ai-interviewer/next.config.js
+++ ai-interviewer/next.config.js
@@ -1,20 +1,4 @@
 /** @type {import("next").NextConfig} */
-const config = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://localhost:5000/api/:path*',
-      },
-    ];
-  },
-  // Add this to enable App Router
-  experimental: {
-    // @ts-ignore
-    appDir: true,
-  },
-};
-
 const nextConfig = {
   reactStrictMode: true,
   typescript: {
@@ -26,5 +10,3 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
-
-export default config;
